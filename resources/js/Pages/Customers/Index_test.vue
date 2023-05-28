bc6c6f39056a4d62c41fd3bc7c8c6e1af413bdb3
Yuri-0708 <yuuri2313@gmail.com>
2023-05-29 00:19:40 +0900

    customers

---
+++ resources/js/Pages/Customers/Index_test.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import Pagination from '@/Components/Pagination.vue';
+    import { onMounted } from 'vue';
+
+    const props = defineProps({
+        customers: Object
+    })
+
+    onMounted(()=> {
+        console.log(props.customers);
+    })
+
+</script>
+
+<template>
+    <ul v-for="customer in customers.data" :key="customer.id">
+        <li>{{ customer.id }}</li>
+        <li>{{ customer.name }}</li>
+    </ul>
+    <Pagination class="mt-6" :links="customers.links"></Pagination>
+</template>
